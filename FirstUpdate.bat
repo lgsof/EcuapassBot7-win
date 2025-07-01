27a38efd2087101923fa66ac15e9b63b2fad98b4
Luis Garreta <luisgarreta@yahoo.com>
2025-07-01 17:47:53 -0500

    Jul/01: r0.9862: Working on Install https COREDB::config mingit.

--- FirstUpdate.bat
+++ FirstUpdate.bat
@@ -1,8 +1,29 @@
-:: Add mingit to PATH
-set PATH=%~dp0mingit/cmd
+@echo off
+setlocal
 
-:: Initialize git repo and fetch latest release
-git init
-git remote add origin https://github.com/lgsof/EcuapassBot7-wintest.git
+:: 1) Add MinGit to PATH
+set "PATH=%~dp0mingit\cmd;%PATH%"
+
+:: 2) First run: init + fetch + hard-reset
+if not exist "%~dp0.git" (
+    echo Inicializando repositorio Git...
+    git init
+    git remote add origin https://github.com/lgsof/EcuapassBot7-win.git
+    echo Descargando y aplicando version inicial...
+    git fetch origin main
+    git reset --hard origin/main
+)
+
+:: 3) Protect all mingit files from future updates
+echo Ignorando futuras actualizaciones de mingit...
+for /f "delims=" %%F in ('git ls-files "mingit/*"') do (
+    git update-index --skip-worktree "%%F"
+)
+
+:: 4) Always pull latest for everything else
+echo Actualizando aplicacion...
 git fetch origin main
 git reset --hard origin/main
+
+endlocal
+
