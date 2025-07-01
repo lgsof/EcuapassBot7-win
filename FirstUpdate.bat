11b5e8b6fcffa894331d237511c3c73a02ac7c64
Luis Garreta <luisgarreta@yahoo.com>
2025-07-01 17:59:26 -0500

    Jul/01: r0.9862: Working on Install https COREDB::config mingit.

--- FirstUpdate.bat
+++ FirstUpdate.bat
@@ -1,31 +1,29 @@
 @echo off
 setlocal
 
-REM 1) Add MinGit to PATH
+:: 1) Put your bundled MinGit on the PATH
 set "PATH=%~dp0mingit\cmd;%PATH%"
 
-REM 2) First run: init + fetch + mixed reset to populate index only
-if not exist "%~dp0.git" (
-    echo [First run] Initializing Git repository...
-    git init
-    git remote add origin https://github.com/lgsof/EcuapassBot7-wintest.git
+:: 2) Check for first run
+if not exist "%~dp0.git" goto :FirstRun
+goto :DoUpdate
 
-    echo [First run] Fetching origin/main...
-    git fetch origin main
+:FirstRun
+echo [First run] Initializing Git repository...
+git init
+git remote add origin https://github.com/lgsof/EcuapassBot7-win.git
+echo [First run] Fetching origin/main...
+git fetch origin main
+git reset --hard origin/main
 
-    echo [First run] Populating index (mixed reset)...
-    git reset --mixed origin/main
+:DoUpdate
+echo Protecting mingit/ from updates...
+for /f "delims=" %%F in ('git ls-files mingit') do git update-index --skip-worktree "%%F"
 
-    echo [First run] Marking mingit files as skip-worktree...
-    for /f "delims=" %%F in ('git ls-files mingit') do (
-        git update-index --skip-worktree "%%F"
-    )
-)
-
-REM 3) Every run: fetch & hard reset (mingit/* will be skipped)
 echo Updating application...
 git fetch origin main
 git reset --hard origin/main
 
 endlocal
+exit /b
 
