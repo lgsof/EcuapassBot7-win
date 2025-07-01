131882fdfb7390c793fec1d64f41d09ea3ed1b4b
Luis Garreta <luis.garreta@yahoo.com>
2025-07-01 10:20:49 -0500

    Jul/01: r0.9862: Working on Install https COREDB::config mingit.

--- FirstUpdate.bat
+++ FirstUpdate.bat
@@ -3,6 +3,6 @@ set PATH=%~dp0mingit/cmd
 
 :: Initialize git repo and fetch latest release
 git init
-git remote add origin https://github.com/lgarreta/EcuapassBot7-wintest.git
+git remote add origin https://github.com/lgsof/EcuapassBot7-wintest.git
 git fetch origin main
 git reset --hard origin/main
